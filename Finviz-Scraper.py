19e7d5604bcf7201f74879ba21d0748d182be9e2
RudolfTheOne <marcin.szymczak@gmail.com>
2020-02-01 18:52:08 +0100

    Added gurufocus F-, Z- and M-Scores. These 3 indicators are appended at the end of dataset (cvs). Extended tickers list.

--- Finviz-Scraper.py
+++ Finviz-Scraper.py
@@ -2,52 +2,101 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import progressbar
+import re
+
+import time
 
 
 def scrape_finviz(symbols):
     # Get Column Header
     req = requests.get("https://finviz.com/quote.ashx?t=FB")
     soup = BeautifulSoup(req.content, 'html.parser')
-    table = soup.find_all(lambda tag: tag.name=='table')
-    rows = table[8].findAll(lambda tag: tag.name=='tr')
-    out=[]
+    table = soup.find_all(lambda tag: tag.name == 'table')
+    rows = table[8].findAll(lambda tag: tag.name == 'tr')
+    out = []
     for i in range(len(rows)):
-        td=rows[i].find_all('td')
-        out=out+[x.text for x in td]
+        td = rows[i].find_all('td')
+        out = out + [x.text for x in td]
 
-    ls=['Ticker','Sector','Sub-Sector','Country']+out[::2]
+    # Adding actual headers: Ticker, Sector, Sub-sector and country plus all from the finviz table
+    # and then 3 from GuruFocus
+    guru_ls = ['Piotroski F-Score', 'Altman Z-Score', 'Beneish M-Score']
+    ls = ['Ticker', 'Sector', 'Sub-Sector', 'Country'] + out[::2] + guru_ls
 
-    dict_ls={k:ls[k] for k in range(len(ls))}
-    df=pd.DataFrame()
+    dict_ls = {k: ls[k] for k in range(len(ls))}
+    df = pd.DataFrame()
     p = progressbar.ProgressBar()
     p.start()
     for j in range(len(symbols)):
-        p.update(j/len(symbols) * 100)
-        req = requests.get("https://finviz.com/quote.ashx?t="+symbols[j])
-        if req.status_code !=200:
+        p.update(j / len(symbols) * 100)
+
+        # Initialize FinViz parsing
+        req = requests.get("https://finviz.com/quote.ashx?t=" + symbols[j])
+        if req.status_code != 200:
             continue
         soup = BeautifulSoup(req.content, 'html.parser')
-        table = soup.find_all(lambda tag: tag.name=='table')
+        table = soup.find_all(lambda tag: tag.name == 'table')
 
-        rows=table[6].findAll(lambda tag: tag.name=='tr')
-        sector=[]
+        # Initialize GuruFocus table parsing (3 values for all the symbols)
+        if (symbols[j].find('-')):
+            guru_symbol = symbols[j].replace('-', '.')
+        guru_req = requests.get("https://www.gurufocus.com/stock/" + guru_symbol)
+        if guru_req.status_code != 200:
+            continue
+        guru_soup = BeautifulSoup(guru_req.content, 'html.parser')
+
+        # Process tables from BS
+        rows = table[6].findAll(lambda tag: tag.name == 'tr')
+        sector = []
         for i in range(len(rows)):
-            td=rows[i].find_all('td')
-            sector=sector+[x.text for x in td]
-        sector=sector[2].split('|')
-        rows = table[8].findAll(lambda tag: tag.name=='tr')
-        out=[]
+            td = rows[i].find_all('td')
+            sector = sector + [x.text for x in td]
+        sector = sector[2].split('|')
+        rows = table[8].findAll(lambda tag: tag.name == 'tr')
+        out = []
         for i in range(len(rows)):
-            td=rows[i].find_all('td')
-            out=out+[x.text for x in td]
-        out=[symbols[j]]+sector+out[1::2]
-        out_df=pd.DataFrame(out).transpose()
-        df=df.append(out_df,ignore_index=True)
+            td = rows[i].find_all('td')
+            out = out + [x.text for x in td]
+        out = [symbols[j]] + sector + out[1::2]
+
+        out_df = pd.DataFrame(out).transpose()
+        df = df.append(out_df, ignore_index=True)
+
+        scores = []
+
+        for val in guru_ls[0:]:
+            try:
+                scores.append(guru_soup.find('a', string=re.compile(val)).find_next('td').text)
+            except:
+                scores.append('')
+
+        df_len = len(df) - 1
+
+        df.loc[df_len, guru_ls[0]] = scores[0]
+        df.loc[df_len, guru_ls[1]] = scores[1]
+        df.loc[df_len, guru_ls[2]] = scores[2]
 
     p.finish()
-    df=df.rename(columns=dict_ls)
+    df = df.rename(columns=dict_ls)
+    df.to_csv('output.csv')
 
-    return(df)
+    return (df)
 
 
-data=scrape_finviz(['FB','INGN','BABA','RMD','GOOS'])
+data = scrape_finviz(['BKNG', 'REGN', 'ceo', 'SPGI', 'AAPL', 'FB', 'GOOGL', 'ISRG', 'INTC', 'ITW', 'MSFT', 'anss', 'ROP',
+                     'ACN', 'IPGP', 'bsm', 'GWW', 'CAT', 'mcd', 'SPR', 'MMM', 'LLY', 'csl', 'MNST', 'hon', 'TWTR',
+                     'NVDA', 'pep', 'JNJ', 'tdg', 'rost', 'IBM', 'BRK-A', 'ndsn', 'OSK', 'ABBV', 'ssw', 'CLX', 'leco',
+                     'POOL',
+                     'lanc', 'expd', 'epam', 'GRMN', 'bti', 'lulu', 'CINF', 'sne', 'chrw', 'DIS', 'NEE', 'PSX', 'apd',
+                     'mplx', 'ev', 'SHW', 'cb', 'EEFT', 'CVX', 'lin', 'PFE', 'MKC', 'PPG', 'AFL', 'ess', 'jag', 'DOV',
+                     'brc', 'WEN', 'chd', 'eca', 'EMR', 'bf-b', 'CMCSA', 'GD', 'cmg', 'CPRI', 'ori', 'TGT', 'wst',
+                     'AMZN', 'ED', 'PNR', 'ADP', 'WM', 'BEN', 'ECL', 'alb', 'tjx', 'BEAT', 'pii', 'ko', 'fast', 'utx',
+                     'cf',
+                     'FRT', 'WBA', 'FCX', 'CCL', 'jw-a', 'VFC', 'CAH', 'ato', 'EXPE', 'nav', 'HRL', 'nvt', 'skt', 'msa',
+                     'sjm', 'dci', 'WMT', 'ful', 'SYY', 'nfg', 'SWK', 'cdk', 'GPC', 'bpy', 'pbct', 'NNN', 'bkh', 'awr',
+                     'O', 'atr', 'son', 'LOW', 'LEG', 'XOM', 'byd', 'ABT', 'BA', 'abm', 'NUE', 'UAA', 'bll', 'BDX',
+                     'crm', 'sfix', 'PG', 'CL', 'ugi', 'cwt', 'wtr', 'njr', 'adm', 'MDT', 'ktb', 'mdp', 'mdu', 'tds',
+                     'QSR',
+                     'rpm', 'FLR', 'NKTR', 'KHC', 'CSX', 'NSC', 'AOS', 'KMB', 'appf', 'NRG', 'ipg', 'T', 'CC', 'anet',
+                     'CTAS', 'amcr', 'rtn', 'lmt', 'hii', 'lulu', 'NOC', 'oxy', 'cop', 'eog', 'pxd', 'cxo', 'bmi',
+                     'fele', 'hp', 'jkhy', 'mgee', 'mgrc', 'mo', 'nwn', 'ph', 'scl', 'sjw', 'syk', 'tnc', 'tr', 'uvv'])
